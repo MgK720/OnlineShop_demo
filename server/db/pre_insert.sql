c3517f5f72d52f39f5d3fba070335cc5040681c8
MgK720 <igorhg720@gmail.com>
2023-10-22 20:27:20 +0200

    pre_insert.sql

---
+++ server/db/pre_insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO order_status(order_status_id, order_status_name)
+VALUES (0, 'ordered'),
+       (1, 'sent'),
+       (2, 'received');
+
+INSERT INTO account_type(account_type_id, account_type_name)
+VALUES (0, 'user'),
+       (1, 'owner');
