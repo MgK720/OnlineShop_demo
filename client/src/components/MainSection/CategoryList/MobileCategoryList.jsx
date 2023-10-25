dd5bb51a87f8a09c3115f183f7670dfec61afe56
MgK720 <igorhg720@gmail.com>
2023-10-26 00:13:39 +0200

    MobileCategoryList transition

--- client/src/components/MainSection/CategoryList/MobileCategoryList.jsx
+++ client/src/components/MainSection/CategoryList/MobileCategoryList.jsx
@@ -4,6 +4,11 @@ import FormControl from '@mui/material/FormControl';
 import { useTheme } from '@mui/material/styles';
 import OutlinedInput from '@mui/material/OutlinedInput';
 import InputLabel from '@mui/material/InputLabel';
+import LoadingButton from '@mui/lab/LoadingButton';
+import Fade  from '@mui/material/Fade';
+import { useEffect, useState } from "react";
+//import CircularProgress from '@mui/material/CircularProgress';
+import SaveIcon from '@mui/icons-material/Save';
 
 const ITEM_HEIGHT = 48;
 const ITEM_PADDING_TOP = 8;
@@ -28,25 +33,56 @@ function getStyles(name, value, theme) {
 
 export default function MobileCategoryList({categories, alignment, handleChange}){
     const theme = useTheme();
+    const [isLoading, setIsLoading] = useState(true);
+
+    useEffect(() => {
+      //Todo wywalić tego setTimeouta gdy podepne się pod baze danych
+      if (categories.length > 0) {
+        setTimeout(() => {
+            setIsLoading(false);
+        }, 1000)
+      }
+    }, [categories]); 
+
+    const renderCategorySelect = () => {
+      return (
+        <Fade in={!isLoading} >
+          <FormControl fullWidth>
+          <InputLabel id="categorySelector">Category</InputLabel>
+              <Select
+                  labelId='categorySelector'
+                  value={alignment}
+                  onChange={handleChange}
+                  input={<OutlinedInput label="Category" />}
+                  MenuProps={MenuProps}
+                  sx={{
+                      fontWeight: theme.typography.fontWeightMedium,
+                  }}
+              >
+              {categories.map((elem, index) => (
+                  <MenuItem value={elem} key={index} style={getStyles(elem, alignment, theme)}>
+                  {elem}
+                  </MenuItem>
+              ))}
+              </Select>
+          </FormControl>
+        </Fade>
+      );
+    }
+
+    const renderLoadingSelect = () => {
+      return (
+        <>
+            <LoadingButton loading loadingPosition="start" startIcon={<SaveIcon />} variant="outlined" size="large" sx={{height:56}} key={'blank1'} fullWidth >
+              Loading...
+            </LoadingButton>
+        </>
+      );
+    }
+
     return (
-        <FormControl fullWidth>
-            <InputLabel id="categorySelector">Category</InputLabel>
-                <Select
-                    labelId='categorySelector'
-                    value={alignment}
-                    onChange={handleChange}
-                    input={<OutlinedInput label="Category" />}
-                    MenuProps={MenuProps}
-                    sx={{
-                        fontWeight: theme.typography.fontWeightMedium,
-                    }}
-                >
-                {categories.map((elem, index) => (
-                    <MenuItem value={elem} key={index} style={getStyles(elem, alignment, theme)}>
-                    {elem}
-                    </MenuItem>
-                ))}
-                </Select>
-        </FormControl>
+        <>
+        {isLoading ?  renderLoadingSelect() : renderCategorySelect() }
+        </>
       );
 }
