24c6758f641e48464ebd3f16142985fd7feffe88
MgK720 <igorhg720@gmail.com>
2023-10-28 21:00:39 +0200

    elephantSQL connection

---
+++ server/databaseConnection.js
@@ -0,0 +1,32 @@
+//elephantsql.com
+var pg = require('pg');
+require('dotenv').config({ debug: process.env.DEBUG });
+
+var client = new pg.Client(process.env.DB_URL);
+
+client.connect(function(err) {
+  if(err) {
+    return console.error('could not connect to postgres', err);
+  }
+  client.query('SELECT NOW() AS "theTime"', function(err, result) {
+    if(err) {
+      return console.error('error running query', err);
+    }
+    console.log(result.rows[0].theTime);
+    // >> output: 2018-08-23T14:02:57.117Z
+  });
+
+});
+
+module.exports = client;
+
+// example
+// const client = require('./elephantsql');
+// app.get('/', async (req, res) => {
+//     try {
+//         const results = await client.query('SELECT * FROM accounts');
+//         res.json(results);
+//     } catch (err) {
+//         console.log(err);
+//     }
+// })
--- server/server.js
+++ server/server.js
@@ -2,8 +2,18 @@ const express = require('express')
 const cors = require('cors');
 const bodyParser = require('body-parser')
 const app = express()
-
+const client = require('./databaseConnection');
 const port = 3000
+
+const queryTest = async () => {
+    try{
+        const results = await client.query('SELECT * FROM order_status');
+        console.log(results.rows)
+    }catch (err) {
+        console.log(err);
+    }
+}
+
 app.use(cors({
     origin: 'http://localhost:5173', 
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
@@ -14,8 +24,11 @@ app.use(express.urlencoded({extended: true}));
 
 app.get("/getmsg", (req, res) => {
     res.json({ message: "Hello from Express!" });
-  });
+});
 
+queryTest();
+
+   
 
 app.listen(port, () => {
     console.log(`App running on port ${port}.`)
